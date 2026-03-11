7071aa341779c15313398a4a43c307389ea181c8
pjlkaw <pedrorochabispo@gmail.com>
2026-03-11 14:50:50 -0300

    remoção do font-awesome

--- paginas/ia_texto/script.js
+++ paginas/ia_texto/script.js
@@ -1,7 +1,7 @@
 import "dotenv/config"
 import Groq from "groq-sdk";
 
-const groq = new Groq({ apiKey: process.env.GROQ_API_KEY})
+const groq = new Groq({ apiKey: process.env.GROQ_API_KEY })
 
 export async function getGroqChatCompletion() {
   return groq.chat.completions.create({
