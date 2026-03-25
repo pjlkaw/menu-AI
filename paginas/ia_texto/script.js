7ea92a4aadf006ba1e4982acce5b137f0b783051
pjlkaw <pedrorochabispo@gmail.com>
2026-03-25 09:28:13 -0300

    interrompido, estudar mais antes de continuar

--- paginas/ia_texto/script.js
+++ paginas/ia_texto/script.js
@@ -1,24 +1,41 @@
-import Groq from "groq-sdk";
-import dotenv from "dotenv"
+
+import express from 'express';
+import Groq from "groq-sdk"; // AI
+import dotenv from "dotenv"; //Key no .env
 dotenv.config({ path: "../../.env"})
 
+const app = express()
+app.use(express.json())
+
+//API KEY do .env
 const groq = new Groq({ apiKey: process.env.GROQ_API_KEY })
 
+//Solicitação
 async function getGroqChatCompletion() {
   return groq.chat.completions.create({
     messages: [{
         role: "user",
-        content: "Faça um resumo da segunda guerra mundial"
+        content: mensagem
     }],
     "model": "openai/gpt-oss-20b"
   })
 }
 
-async function main() {
-  const chatCompletion = await getGroqChatCompletion();
-  console.log(chatCompletion.choices[0]?.message?.content || "");
-}
+//Resposta
+app.post('/api/data', async (req,res)=>{
+  try{
+    const { mensagem } = req.body
+    const chatCompletion = await getGroqChatCompletion();
+    const resposta = chatCompletion.choices[0]?.message?.content || "";
+    res.json({ resposta });
+    console.log( resposta );
+  }
+  catch (error){
+    console.log('não é possivel realizar');
+    res.status(500).json({erro: error.message});
+  }
+})
+app.listen('3000', () => { console.log('Servidor rodando na porta 3000')});
 
-main()
 
 
